07743311ad7c8255366532c57ea72c6a53688fad
tristondvs <tristondvs@yahoo.com>
2022-01-09 22:38:42 +0000

    updating

--- twitch_chatbot/bot.py
+++ twitch_chatbot/bot.py
@@ -11,30 +11,6 @@ bot = commands.Bot(
     initial_channels=[os.environ['CHANNEL']]
 )
 
-@bot.event
-async def event_ready():
-    'Called once when the bot goes online.'
-    print(f"{os.environ['BOT_NICK']} is online!")
-    ws = bot._ws  # this is only needed to send messages within event_ready
-    await ws.send_privmsg(os.environ['CHANNEL'], f"/me has landed!")
-
-
-@bot.event
-async def event_message(ctx):
-    'Runs every time a message is sent in chat.'
-
-    # make sure the bot ignores itself and the streamer
-    if ctx.author.name.lower() == os.environ['BOT_NICK'].lower():
-        return
-
-    await bot.handle_commands(ctx)
-
-    # await ctx.channel.send(ctx.content)
-
-    if 'hello' in ctx.content.lower():
-        await ctx.channel.send(f"Hi, @{ctx.author.name}!")
-
-
 @bot.command(name='test')
 async def test(ctx):
     await ctx.send('test passed!')
