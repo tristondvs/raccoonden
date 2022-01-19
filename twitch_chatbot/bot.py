6f2a2a033dca36881e429405ba92099dd4ef5f90
tristondvs <tristondvs@yahoo.com>
2022-01-19 00:21:04 +0000

    added @'s so the bot will tag the user that sent the command

--- twitch_chatbot/bot.py
+++ twitch_chatbot/bot.py
@@ -27,28 +27,28 @@ async def test(ctx, arg='none'):
     p = 2
     s = 3
     if (arg == "none"):
-        await ctx.send('Call the rps command while also making your choice. (example: "!rps paper")')
+        await ctx.send(f'@{ctx.author.name},  be sure to call the !rps command while also making your choice. (example: "!rps paper")')
     if (arg == "rock"):
         if (n == 1):
-            await ctx.send('I also chose rock, so we tied. mormon2RNG')
+            await ctx.send(f'@{ctx.author.name}, I also chose rock, so we tied. mormon2RNG')
         elif (n == 2):
-            await ctx.send('I chose paper, so I win! mormon2LUL')
+            await ctx.send(f'@{ctx.author.name}, I chose paper, so I win! mormon2LUL')
         else:
-            await ctx.send('I chose scissors, so you win! mormon2HYPE')
+            await ctx.send(f'@{ctx.author.name}, I chose scissors, so you win! mormon2HYPE')
     if (arg == "paper"):
         if (n == 1):
-            await ctx.send('I chose rock, so you win! mormon2HYPE')
+            await ctx.send(f'@{ctx.author.name}, I chose rock, so you win! mormon2HYPE')
         if (n == 2):
-            await ctx.send('I also chose paper, so we tied. mormon2RNG')
+            await ctx.send(f'@{ctx.author.name}, I also chose paper, so we tied. mormon2RNG')
         if (n == 3):
-            await ctx.send('I chose scissors, so I win! mormon2LUL')
+            await ctx.send(f'@{ctx.author.name}, I chose scissors, so I win! mormon2LUL')
     if (arg == "scissors"):
         if (n == 1):
-            await ctx.send('I chose rock, so I win! mormon2LUL')
+            await ctx.send(f'@{ctx.author.name}, I chose rock, so I win! mormon2LUL')
         if (n == 2):
-            await ctx.send('I chose paper, so you win! mormon2HYPE')
+            await ctx.send(f'@{ctx.author.name}, I chose paper, so you win! mormon2HYPE')
         if (n == 3):
-            await ctx.send('I also chose scissors, so we tied. mormon2RNG')
+            await ctx.send(f'@{ctx.author.name}, I also chose scissors, so we tied. mormon2RNG')
     #else:
     #    await ctx.send("You must choose rock, paper, or scissors. Play fair! you can't bring " + arg + " to this battle!")
 
