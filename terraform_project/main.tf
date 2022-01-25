83fb004a2168490d8af28b5f7989003faa637e86
tristondvs <tristondvs@yahoo.com>
2022-01-25 22:17:11 +0000

    adding sample terraform files used in lab courses for review

---
+++ terraform_project/main.tf
@@ -0,0 +1,19 @@
+variable "main_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+provider "aws" {
+  region = var.main_region
+}
+
+module "vpc" {
+  source = "./modules/vpc"
+  region = var.main_region
+}
+
+resource "aws_instance" "my-instance" {
+  ami           = module.vpc.ami_id
+  subnet_id     = module.vpc.subnet_id
+  instance_type = "t2.micro"
+}
