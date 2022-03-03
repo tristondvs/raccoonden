db11de464f9fd1f55401755c9dfd480aaf5b53a3
tristondvs <tristondvs@yahoo.com>
2022-03-03 02:35:13 +0000

    changed client variable to bot, added commands to join/leave voice, added bot command handler on greeting function

--- discord_bot/bot.py
+++ discord_bot/bot.py
@@ -1,4 +1,5 @@
 import os
+from discord.ext import commands
 import discord
 import random
 from dotenv import load_dotenv
@@ -7,32 +8,56 @@ client_user = 'TrashPandaBot'
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+#client = discord.Client()
+bot = commands.Bot(command_prefix = '$')
 
-@client.event
+@bot.event
 async def on_ready():
     print(f'{client_user} has connected to Discord!')
 
-@client.event
+@bot.event
 async def on_member_join(member):
     await member.create_dm()
     await member.dm_channel.send(
             f'Hello there {member.name}, welcome to Tha Dumpsta!'
     )
 
-@client.event
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         return
 
     greetings = [
     '```From the corner of the dumpster, you hear a faint sound of paper crumpling, followed by the tumbling of soda cans. Two glowing green eyes peer at you from across the garbage...``` Hello there! I\'m not quite sure what this means *exactly*, but I was told to let you know, that... ```The stony, jade colored eyes quickly glance at a greasy, crumpled piece of paper stuck to his bottom paw...``` ...That \"the docker container is now running\"...? Better go catch it!!', 
     '```Your voice rings within the steel abode, and echoes into nothingness. You sense that your friend might be sleeping for the time being.```', 
-    '```Feeling a fuzzy object quickly brush against your right elbow, you quickly turn around in shock and are greeted with a friendly face...``` I didn\'t mean to sneak up on you there, I was over here organizing my bottlecap collection.'
+    '```Feeling a fuzzy object quickly brush against your right elbow, you quickly turn around in shock and are greeted with a friendly face...``` I didn\'t mean to sneak up on you there, I was over here organizing my bottlecap collection.', 
+    '```A dark figure from the other side of the dumpster turns to greet you. Your fuzzy friend raises one hand in greeting, while the other holds a half-eaten slice of pizza. He continues to chew with his mouth closed; we still practice table manners here. Nevertheless, you feel welcome and acknowledged.```'
     ]
 
     if message.content == 'testing':
         response = random.choice(greetings)
         await message.channel.send(response)
 
-client.run(TOKEN)
+    # process all the bot commands sent here, since the on_message event gets processed with every single message
+    await bot.process_commands(message)
+
+@bot.command()
+async def join(ctx):
+    # If a user is within a voice channel:
+    if (ctx.author.voice):
+        channel = ctx.message.author.voice.channel
+        await channel.connect()
+    else:
+        await ctx.send(
+        "```Your fuzzy friend's ears perk up to your request; he looks around but can't make out where the voice came from. He seems to not know which room you're in...```"
+        )
+        
+@bot.command()
+async def leave(ctx):
+    if (ctx.voice_client):
+        await ctx.guild.voice_client.disconnect()
+        await ctx.send("Okay, I'm headed out. See you next time!")
+    else:
+        await ctx.send("```You command echoes throughout the room with no response. Your fuzzy friend is not in a voice channel.```")
+
+bot.run(TOKEN)
