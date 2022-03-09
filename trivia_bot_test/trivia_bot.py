c23a57e5ad3eaaf991ec5c84c42ee00e1a9d6391
tristondvs <tristondvs@yahoo.com>
2022-03-09 05:01:27 +0000

    updated TODO, changed category to use list, added some notes for posterity

--- trivia_bot_test/trivia_bot.py
+++ trivia_bot_test/trivia_bot.py
@@ -6,16 +6,19 @@ import urllib.parse, json, requests, random, time, html
 h = html.unescape
 # defining game options, like the number of questions and what type of questions they will be
 num_of_ques=1
-category=15
+# changing category to list; allowing the context of categories to change for each question (if desirable)
+# this should be called in a function; doesn't work like it is now
+available_categories=[15]
+category = random.choice(available_categories)
 base_url = f'https://opentdb.com/api.php?amount={num_of_ques}&category={category}'
 url = requests.get(base_url)
 data = url.content
-# data below contains entire json formatted trivia question
 
 # define number of players in function
+# a short time where players can '!join' to be the ones ones able to answer or earn points
 
 # start game using data and player count
-def game_play(data, players=1, points=0, total_rounds=5):
+def game_play(data, players=1, points=0, total_rounds=3):
     current_round = 1 
     while current_round <= total_rounds:
         url = requests.get(base_url)
@@ -30,7 +33,7 @@ def game_play(data, players=1, points=0, total_rounds=5):
         answers.append(correct_ans)
         answer_choices = h(random.sample(answers, k=len(answers)))
         print(f'Question number {current_round} is a {question_d} {question_t} choice question:')
-        # small wait to give people time to read the question info above
+        # small wait to give people time to read the question info above, maybe change to configurable value
         time.sleep(3)
         print(question)
         # another small wait here, quicker if the question is True or False
@@ -44,8 +47,7 @@ def game_play(data, players=1, points=0, total_rounds=5):
         current_round += 1
         continue
     
-
-        #evaluate answer and award points
+        # evaluate answer and award points
 
 
 # call game function to start the round
@@ -53,11 +55,11 @@ game_play(data)
 
 
 ### TODO
-# give the ability to define player entries (ie a join command)
-# evaluate answer to determine if answer was correct or not
-# stop waiting once all players have answered
+# define async functions, as most of these can only be tested that way
+# async - give the ability to define player entries (ie a join command)
+# async - evaluate answer to determine if answer was correct or not
+# async - stop waiting once all players have answered
 # determine who answered correctly first
 # points system for correct answers, more points for answering quicker
-# test ability to pass through multiple questions
 # determine winner after all questions are completed
 # give ability to randomize category, or choose categories to add to list to bounce in between
