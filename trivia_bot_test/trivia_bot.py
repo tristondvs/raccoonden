28045131e2b9faff7c6f4cdaeb75a91e9d45bb92
tristondvs <tristondvs@yahoo.com>
2022-03-09 04:36:50 +0000

    fixed html character encoding in questions/answers

--- trivia_bot_test/trivia_bot.py
+++ trivia_bot_test/trivia_bot.py
@@ -1,13 +1,15 @@
-import urllib.parse
-import json, requests, random, time
+from html.parser import HTMLParser
+import urllib.parse, json, requests, random, time, html
 
+# hopefully fixing character encoding problems wwith questions and answers
+# edit: It does :D Finally
+h = html.unescape
 # defining game options, like the number of questions and what type of questions they will be
-
 num_of_ques=1
 category=15
 base_url = f'https://opentdb.com/api.php?amount={num_of_ques}&category={category}'
 url = requests.get(base_url)
-data = url.json()
+data = url.content
 # data below contains entire json formatted trivia question
 
 # define number of players in function
@@ -22,16 +24,15 @@ def game_play(data, players=1, points=0, total_rounds=5):
         question_info = question_info_list[0]
         question_d = question_info['difficulty']
         question_t = question_info['type']
-        question = question_info['question']
-        ques = question.encode()
+        question = h(question_info['question'])
         answers = list(question_info['incorrect_answers'])
         correct_ans = (question_info['correct_answer'])
         answers.append(correct_ans)
-        answer_choices = random.sample(answers, k=len(answers))
+        answer_choices = h(random.sample(answers, k=len(answers)))
         print(f'Question number {current_round} is a {question_d} {question_t} choice question:')
         # small wait to give people time to read the question info above
-        time.sleep(5)
-        print(str(ques))
+        time.sleep(3)
+        print(question)
         # another small wait here, quicker if the question is True or False
         if len(answer_choices) == 2:
             time.sleep(3)
@@ -52,10 +53,11 @@ game_play(data)
 
 
 ### TODO
-# fix character encoding in question/answer strings
 # give the ability to define player entries (ie a join command)
 # evaluate answer to determine if answer was correct or not
+# stop waiting once all players have answered
 # determine who answered correctly first
 # points system for correct answers, more points for answering quicker
 # test ability to pass through multiple questions
 # determine winner after all questions are completed
+# give ability to randomize category, or choose categories to add to list to bounce in between
