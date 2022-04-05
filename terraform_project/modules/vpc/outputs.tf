9494bc9c4ba9bf38449593e96ce7f9470dbc3070
tristondvs <tristondvs@yahoo.com>
2022-04-05 23:15:59 +0000

    removing old terraform files

--- terraform_project/modules/vpc/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "subnet_id" {
-  value = aws_subnet.this.id
-}
-
-output "ami_id" {
-  value = data.aws_ssm_parameter.this.value
-}
--- terraform_project/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "PrivateIP" {
-  description = "Private IP of EC2 instance"
-  value       = aws_instance.my-instance.private_ip
-}
