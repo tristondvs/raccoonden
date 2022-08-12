cd297626e6c574c6251179d22711f80fc849236c
tristondvs <77812360+tristondvs@users.noreply.github.com>
2022-08-11 20:54:36 -0500

    Update script.js

--- voice-visualizer/script.js
+++ voice-visualizer/script.js
@@ -1,26 +1,73 @@
-var r, g, b;
-var mic;
+//very crude pitch detection
+// created by datramnt https://editor.p5js.org/datramt/sketches/rkC5INGBQ
 
-r = 255;
-g = 0;
-b = 0;
+// source mic audio from browser source
+let mic;
+let fft;
+let energies = [];
+// adding vars for chroma key value and mousePressed Function
+let r, g, b;
+let chroma = "0,177,64";
 
 function setup() {
-  createCanvas(500, 500);
-
+  createCanvas(800, 400);
   mic = new p5.AudioIn();
   mic.start();
+  fft = new p5.FFT();
+  fft.setInput(mic);
+  noStroke();
+  fill("red");
+}
+
+// changes color of spectrum on mouse-click
+function mousePressed() {
+  r = random(255);
+  g = random(255);
+  b = random(255);
 }
 
 function draw() {
-  var micLevel = mic.getLevel();
-  background(220);
+  background(chroma);
   fill(r, g, b);
-  ellipse(250, 250, 50 + micLevel * 2000);
+  let spectrum = fft.analyze();
+
+  for (let i = 0; i < 127; i++) {
+    energies[i] = fft.getEnergy(midiToFreq(i));
+  }
+
+  //at what index of energies is the max?
+  let indexOfMaxValue = indexOfMax(energies);
+
+  // console.log(indexOfMaxValue)
+
+  beginShape();
+  vertex(0, height);
+  for (let i = 0; i < energies.length; i++) {
+    vertex((i * width) / energies.length, height - energies[i]);
+  }
+  vertex(width, height);
+  endShape();
+
+  let micLevel = mic.getLevel();
+  // hiding mic audio circle reference
+  //fill('red');-
+  //ellipse(indexOfMaxValue*width/127, constrain(height-micLevel*height*5, 0, height), 30);
 }
 
-function mousePressed() {
-  r = random(256);
-  g = random(256);
-  b = random(256);
+function indexOfMax(arr) {
+  if (arr.length === 0) {
+    return -1;
+  }
+
+  var max = arr[0];
+  var maxIndex = 0;
+
+  for (var i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      maxIndex = i;
+      max = arr[i];
+    }
+  }
+
+  return maxIndex;
 }
