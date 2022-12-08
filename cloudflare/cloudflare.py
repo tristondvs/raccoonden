65577d2d5649876f24ae313ee9e301b39599c481
tristondvs <tristondvs@yahoo.com>
2022-12-08 16:55:17 -0600

    adding working cloudflare

--- cloudflare/cloudflare.py
+++ cloudflare/cloudflare.py
@@ -1,12 +1,16 @@
-import requests, os, json
+import requests, os, json, sys
 from dotenv import load_dotenv
 
 ### load env vars from .env
+### can we load these credentials in aws parameter store?
 load_dotenv()
 
 email = os.getenv('USER_EMAIL')
 apikey = os.getenv('API_KEY')
 zone = os.getenv('ZONE_ID')
+pool_ids = ['d058132d42378dd050b239e9a365e45d', '71399e3cbf69c3850a0adf00084b59e8']
+pool_nick = sys.argv[1]
+action = sys.argv[2]
 
 headers = {
     'X-Auth-Email': email,
@@ -14,83 +18,38 @@ headers = {
     'Content-Type': 'application/json',
 }
 
-### determine list of pool names, list all pool ids for zone
-
-def fetch_pools():
-  response = requests.get('https://api.cloudflare.com/client/v4/accounts/' + zone + '/load_balancers/pools', headers=headers)
-  j = response.json()
-  if j["success"]:
-    global pool_id
-    pool_id = []
-    for i in range(len(j["result"])):
-      pool_id.append(j["result"][i]["id"])
-  else: #print the errors returned if request fails
-      print(j["errors"])
-  print('Determined available pool ids: ' + str(pool_id))
-
-### query status of all pools, determine if enabled or disabled
-
-def fetch_pool_status(pool_id):
-  #print(len(pool_id))
-  global pool_status
+### new function definition here
+def main(pool_nick, action):
   pool_status = {}
-  for i in range(len(pool_id)):
-    response = requests.get('https://api.cloudflare.com/client/v4/accounts/' + zone + '/load_balancers/pools/' + pool_id[i], headers=headers)
-    j = response.json()
-    if j["success"]:
+    # determine pool id for provided pool name, query aws parameter store for json object?
+    # for now we manually provide the json object below and parse it
+  for i in range(len(pool_ids)):
+    try:
+      response = requests.get('https://api.cloudflare.com/client/v4/accounts/' + zone + '/load_balancers/pools/' + pool_ids[i], headers=headers)
+      j = response.json()
       pool_status[i] = {
-                "pool_id": j["result"]["id"],
-                "pool_name": j["result"]["name"],
-                "enabled": str(j["result"]["enabled"])
-              }
-    else:
-      print(j["errors"])
-
-### user input: choose pool and determine action
-
-def user_choose_pool(pool_status):
-  for i in range(len(pool_status)):
-    print(i, pool_status[i])
-  while True:
-    global pool_choice
-    pool_choice = input('Choose available pool index value above: ')
-    if int(pool_choice) in range(len(pool_status)):
-      global pool_chosen
-      pool_chosen = int(pool_choice)
-      break
-    else:
-      print('Pool selection not available')
-
-def change_pool_status(pool_status, pool_chosen):
-  c = pool_chosen
-  e = pool_status[c]["enabled"]
-  p = pool_status[c]["pool_name"]
-  if e == "True":
-    confirm = input('Pool ' + p + ' will be disabled, continue? (y/n): ')
-    while True:
-      if confirm == "y":
-        data = '{"enabled": false}'
-        pool_state = requests.patch('https://api.cloudflare.com/client/v4/accounts/' + zone + '/load_balancers/pools/' + pool_id[c], headers=headers, data=data)
-        break
-      elif confirm == "n":
-        print('Exiting process, changes have not been made.')
-        exit()
-  elif e == "False":
-    confirm = input('Pool ' + p + ' will be enabled, continue? (y/n): ')
-    while True:
-      if confirm == "y":
+                  "pool_id": j["result"]["id"],
+                  "pool_name": j["result"]["id"]
+                  }
+      if pool_status[i]["pool_name"] == pool_nick:
+        global pool
+        pool = pool_status["pool_id"]
+      if action == 'enable':
         data = '{"enabled": true}'
-        pool_state = requests.patch('https://api.cloudflare.com/client/v4/accounts/' + zone + '/load_balancers/pools/' + pool_id[c], headers=headers, data=data)
-        break
-      elif confirm == "n":
-        print('Exiting process, changes have not been made.')
-        exit()
+      elif action == 'disable':
+        data = '{"enabled": false}'
 
+      if pool_status[i]["pool_name"] == pool_nick:
+        pool = pool_status["pool_id"]
+        print(zone, pool, data)
+        requests.patch('https://api.cloudflare.com/client/v4/accounts/' + zone + '/load_balancers/pools/' + pool, headers=headers, data=data)
 
+### determine list of pool names, list all pool ids for zone
+### can we determine the pool ids and load them in aws parameter store? These should ideally never change unless torn down completely
+### query status of all pools, determine if enabled or disabled
+### does this step even matter; can we enable an already enabled pool without issue?
+### can we force an action as mentioned above and avoid a choice?
 ### check status of pools after changes to verify
-    
-fetch_pools()
-fetch_pool_status(pool_id)
-user_choose_pool(pool_status)
-change_pool_status(pool_status, pool_chosen)
-fetch_pool_status(pool_id)
+### can we also write this state elsewhere, so we can confirm the state without accessing cloudflare?
+if __name__ == "__main__":
+    main(pool_nick, action)
